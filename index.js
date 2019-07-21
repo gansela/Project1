95c6c8114a4c26d302676ae5791fe96254cf3469
gansela <gan.sela@gmail.com>
2019-07-21 17:59:28 +0300

    sticky note form

---
+++ index.js
@@ -0,0 +1,10 @@
+// 1. model dom
+
+const NOTES_DOM = {
+    taskName: document.getElementById("task-name"),
+    taskDetailes: document.getElementById("task-details"),
+    taskDate: document.getElementById("task-date"),
+    taskTime: document.getElementById("task-time"),
+
+    divData: document.getElementById("data-div"),
+}
